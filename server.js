f0227925ce5159da89513fb9612b589f4e4f04cd
dissonance <ryanminor01u@yahoo.com>
2019-03-20 17:12:12 -0500

    trimmed down necessary components, skeleton layout being worked on

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// Define middleware here
+// middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // Serve up static assets (usually on heroku)
