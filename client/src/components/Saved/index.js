aed5af17a1e803a6cfe6a02387f2e21466e36424
dissonance <ryanminor01u@yahoo.com>
2019-03-22 21:13:02 -0500

    star ratings functional

--- client/src/components/Saved/index.js
+++ client/src/components/Saved/index.js
@@ -1,83 +1,86 @@
 import React from "react";
 import coverArt from '../../images/matrix.png';
 
-
 function Saved() {
     return (
         <div className="ui grid">
-            <div className="ui four cards">
-                <div className="card">
-                    <div className="image">
-                        <img src={coverArt} alt="saved-movie" />
+            <div className="two wide column"></div>
+            <div className="twelve wide column">
+                <div className="ui four cards">
+                    <div className="card">
+                        <div className="image">
+                            <img src={coverArt} alt="movie-art" />
+                        </div>
+                        <div className="extra">
+                            Rating:
+                            <div className="ui star rating" data-rating="0"></div>
+                        </div>
                     </div>
-                    <div className="extra">
-                        Rating:
-      <div className="ui star rating" data-rating="4"></div>
+                    <div className="card">
+                        <div className="image">
+                            <img src={coverArt} alt="movie-art" />
+                        </div>
+                        <div className="extra">
+                            Rating:
+                            <div className="ui star rating" data-rating="0"></div>
+                        </div>
+                    </div>
+                    <div className="card">
+                        <div className="image">
+                            <img src={coverArt} alt="movie-art" />
+                        </div>
+                        <div className="extra">
+                            Rating:
+                            <div className="ui star rating" data-rating="0"></div>
+                        </div>
+                    </div>
+                    <div className="card">
+                        <div className="image">
+                            <img src={coverArt} alt="movie-art" />
+                        </div>
+                        <div className="extra">
+                            Rating:
+                            <div className="ui star rating" data-rating="0"></div>
+                        </div>
+                    </div>
+                    <div className="card">
+                        <div className="image">
+                            <img src={coverArt} alt="movie-art" />
+                        </div>
+                        <div className="extra">
+                            Rating:
+                            <div className="ui star rating" data-rating="0"></div>
+                        </div>
+                    </div>
+                    <div className="card">
+                        <div className="image">
+                            <img src={coverArt} alt="movie-art" />
+                        </div>
+                        <div className="extra">
+                            Rating:
+                            <div className="ui star rating" data-rating="0"></div>
+                        </div>
+                    </div>
+                    <div className="card">
+                        <div className="image">
+                            <img src={coverArt} alt="movie-art" />
+                        </div>
+                        <div className="extra">
+                            Rating:
+                            <div className="ui star rating" data-rating="0"></div>
+                        </div>
+                    </div>
+                    <div className="card">
+                        <div className="image">
+                            <img src={coverArt} alt="movie-art" />
+                        </div>
+                        <div className="extra">
+                            Rating:
+                            <div className="ui star rating" data-rating="0"></div>
+                        </div>
                     </div>
                 </div>
-                <div className="card">
-                    <div className="image">
-                        <img src={coverArt} alt="saved-movie" />
-                    </div>
-                    <div className="extra">
-                        Rating:
-      <div className="ui star rating" data-rating="2"></div>
-                    </div>
-                </div>
-                <div className="card">
-                    <div className="image">
-                        <img src={coverArt} alt="saved-movie" />
-                    </div>
-                    <div className="extra">
-                        Rating:
-      <div className="ui star rating" data-rating="3"></div>
-                    </div>
-                </div>
-                <div className="card">
-                    <div className="image">
-                        <img src={coverArt} alt="saved-movie" />
-                    </div>
-                    <div className="extra">
-                        Rating:
-      <div className="ui star rating" data-rating="4"></div>
-                    </div>
-                </div>
-                <div className="card">
-                    <div className="image">
-                        <img src={coverArt} alt="saved-movie" />
-                    </div>
-                    <div className="extra">
-                        Rating:
-      <div className="ui star rating" data-rating="3"></div>
-                    </div>
-                </div>
-                <div className="card">
-                    <div className="image">
-                        <img src={coverArt} alt="saved-movie" />
-                    </div>
-                    <div className="extra">
-                        Rating:
-      <div className="ui star rating" data-rating="3"></div>
-                    </div>
-                </div>
-                <div className="card">
-                    <div className="image">
-                        <img src={coverArt} alt="saved-movie" />
-                    </div>
-                    <div className="extra">
-                        Rating:
-      <div className="ui star rating" data-rating="4"></div>
-                    </div>
-                </div>
-                <div className="card">
-                    <div className="image">
-                        <img src={coverArt} alt="saved-movie" />
-                    </div>
-                    <div className="extra">
-                        Rating:
-      <div className="ui star rating" data-rating="4"></div>
-                    </div>
-                </div>
+
             </div>
         </div>
     );
