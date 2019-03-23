95a63fad4b407f717b6a1ac4d4ee07db8355c705
dissonance <ryanminor01u@yahoo.com>
2019-03-22 19:51:20 -0500

    cards in saved movies/npm issues solved

--- client/src/components/Saved/index.js
+++ client/src/components/Saved/index.js
@@ -5,77 +5,77 @@ import coverArt from '../../images/matrix.png';
 function Saved() {
     return (
         <div className="ui grid">
-            <div class="ui four cards">
-                <div class="card">
-                    <div class="image">
+            <div className="ui four cards">
+                <div className="card">
+                    <div className="image">
                         <img src={coverArt} alt="saved-movie" />
                     </div>
-                    <div class="extra">
+                    <div className="extra">
                         Rating:
-      <div class="ui star rating" data-rating="4"></div>
+      <div className="ui star rating" data-rating="4"></div>
                     </div>
                 </div>
-                <div class="card">
-                    <div class="image">
+                <div className="card">
+                    <div className="image">
                         <img src={coverArt} alt="saved-movie" />
                     </div>
-                    <div class="extra">
+                    <div className="extra">
                         Rating:
-      <div class="ui star rating" data-rating="2"></div>
+      <div className="ui star rating" data-rating="2"></div>
                     </div>
                 </div>
-                <div class="card">
-                    <div class="image">
+                <div className="card">
+                    <div className="image">
                         <img src={coverArt} alt="saved-movie" />
                     </div>
-                    <div class="extra">
+                    <div className="extra">
                         Rating:
-      <div class="ui star rating" data-rating="3"></div>
+      <div className="ui star rating" data-rating="3"></div>
                     </div>
                 </div>
-                <div class="card">
-                    <div class="image">
+                <div className="card">
+                    <div className="image">
                         <img src={coverArt} alt="saved-movie" />
                     </div>
-                    <div class="extra">
+                    <div className="extra">
                         Rating:
-      <div class="ui star rating" data-rating="4"></div>
+      <div className="ui star rating" data-rating="4"></div>
                     </div>
                 </div>
-                <div class="card">
-                    <div class="image">
+                <div className="card">
+                    <div className="image">
                         <img src={coverArt} alt="saved-movie" />
                     </div>
-                    <div class="extra">
+                    <div className="extra">
                         Rating:
-      <div class="ui star rating" data-rating="3"></div>
+      <div className="ui star rating" data-rating="3"></div>
                     </div>
                 </div>
-                <div class="card">
-                    <div class="image">
+                <div className="card">
+                    <div className="image">
                         <img src={coverArt} alt="saved-movie" />
                     </div>
-                    <div class="extra">
+                    <div className="extra">
                         Rating:
-      <div class="ui star rating" data-rating="3"></div>
+      <div className="ui star rating" data-rating="3"></div>
                     </div>
                 </div>
-                <div class="card">
-                    <div class="image">
+                <div className="card">
+                    <div className="image">
                         <img src={coverArt} alt="saved-movie" />
                     </div>
-                    <div class="extra">
+                    <div className="extra">
                         Rating:
-      <div class="ui star rating" data-rating="4"></div>
+      <div className="ui star rating" data-rating="4"></div>
                     </div>
                 </div>
-                <div class="card">
-                    <div class="image">
+                <div className="card">
+                    <div className="image">
                         <img src={coverArt} alt="saved-movie" />
                     </div>
-                    <div class="extra">
+                    <div className="extra">
                         Rating:
-      <div class="ui star rating" data-rating="4"></div>
+      <div className="ui star rating" data-rating="4"></div>
                     </div>
                 </div>
             </div>
