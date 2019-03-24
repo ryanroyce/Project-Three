1ae2f61d45f076d3063b833a7d67e650bde2bb3c
dissonance <ryanminor01u@yahoo.com>
2019-03-24 17:59:08 -0500

    sign-up modal created

--- client/src/components/LoginForm/index.js
+++ client/src/components/LoginForm/index.js
@@ -37,9 +37,55 @@ function LoginForm() {
 
           <div className="ui message">
           <i className="us flag"></i>
-            New to us? <a href="/">Sign Up</a>
+            New to us? 
+            
+            <button class="ui button yellow create_btn" type="button" id="test">Sign Up</button>
+            {/* <a href="/">Sign Up</a> */}
           </div>
        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+          <div class="ui modal test">
+  <i class="close icon"></i>
+  <div class="header">
+    Sign Up for an account
+  </div>
+  <div class="image content">
+    <div class="ui medium image">
+      <img src="https://semantic-ui.com/images/avatar2/large/rachel.png" alt="user-pic" />
+    </div>
+    <div class="description">
+      <div class="ui header">We've auto-chosen a profile image for you.</div>
+      <p>We've grabbed the following image from the <a href="https://www.gravatar.com" target="_blank" rel="noopener noreferrer">gravatar</a> image associated with your registered e-mail address.</p>
+      <p>Is it okay to use this photo?</p>
+    </div>
+  </div>
+  <div class="actions">
+    <div class="ui black deny button">
+      Nope
+    </div>
+    <div class="ui positive right labeled icon button">
+      Yep, that's me
+      <i class="checkmark icon"></i>
+    </div>
+  </div>
+</div>
         </div>
       </div>
   );
