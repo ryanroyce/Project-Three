484e781be6e2e9df4089f3ddda1679b7d75b4db1
dissonance <ryanminor01u@yahoo.com>
2019-03-24 18:36:54 -0500

    signup modal clean up

--- client/src/components/LoginForm/index.js
+++ client/src/components/LoginForm/index.js
@@ -6,12 +6,11 @@ function LoginForm() {
       <div className="ui middle aligned center aligned grid" id="body">
         <div className="six wide column">
           <h2 className="ui black image header">
-
             <div className="content">
               Log-in to your account
             </div>
           </h2>
-
+          {/* login form */}
           <form className="ui large form">
             <div className="ui stacked segment">
               <div className="field">
@@ -39,74 +38,45 @@ function LoginForm() {
           <i className="us flag"></i>
             New to us? 
             
-            <button class="ui button yellow create_btn" type="button" id="test">Sign Up</button>
-            {/* <a href="/">Sign Up</a> */}
+            <button className="ui button yellow create_btn" type="button" id="signup">Sign Up</button>
           </div>
        
+          {/* signup modal */}
+          <div className="ui modal signup-modal">
+            <i className="close icon"></i>
+            <div className="header"> Sign Up for an account</div>
 
+            <div className="image content">
+              <div className="description">
+                {/* sign up form */}
+                <form className="ui large form">
+                  <div className="ui stacked segment">
+                    <div className="field">
+                      <div className="ui left icon input">
+                        <i className="user icon"></i>
+                        <input type="text" name="email" placeholder="E-mail address" />
+                      </div>
+                    </div>
 
+                    <div className="field">
+                      <div className="ui left icon input">
+                        <i className="lock icon"></i>
+                        <input type="password" name="password" placeholder="Password" />
+                      </div>
+                    </div>
+              
+                    <a href="/">
+                    <div className="ui fluid large grey submit button">Sign Up</div>
+                    </a>
+                  </div>
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-          <div class="ui modal test">
-  <i class="close icon"></i>
-  <div class="header">
-    Sign Up for an account
-  </div>
-  <div className="ui row"></div>
-  <div class="image content">
-    <div class="description">
-{/* sign up form */}
-      <form className="ui large form">
-      
-            <div className="ui stacked segment">
-              <div className="field">
-                <div className="ui left icon input">
-                  <i className="user icon"></i>
-                  <input type="text" name="email" placeholder="E-mail address" />
-                </div>
-              </div>
-
-              <div className="field">
-                <div className="ui left icon input">
-                  <i className="lock icon"></i>
-                  <input type="password" name="password" placeholder="Password" />
-                </div>
-              </div>
-              <a href="/">
-              <div className="ui fluid large grey submit button">Sign Up</div>
-              </a>
-            </div>
-
-            <div className="ui error message"></div>
-          </form>
-    </div>
-  </div>
-  {/* <div class="actions">
-    <div class="ui black deny button">
-      Nope
-    </div>
-    <div class="ui positive right labeled icon button">
-      Yep, that's me
-      <i class="checkmark icon"></i>
-    </div>
-  </div> */}
-</div>
-        </div>
-      </div>
+                    <div className="ui error message"></div>
+                 </form>
+               </div>
+             </div>
+           </div>
+         </div>
+       </div>
   );
 }
 
