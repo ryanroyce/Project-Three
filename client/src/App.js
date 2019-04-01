a134db063772dc4efd1df50b6168c381158c57f3
dissonance <ryanminor01u@yahoo.com>
2019-04-01 14:38:32 -0500

    /signup routing fixed

--- client/src/App.js
+++ client/src/App.js
@@ -7,14 +7,14 @@ import SavedMovies from "./pages/SavedMovies";
 import Settings from "./pages/Settings";
 // import Logout from "./pages/Logout";
 import NoMatch from "./pages/NoMatch";
-import Signup from "./pages/sign-up"
+import Signup from "./pages/SignUp"
 import Success from "./pages/Success";
 import LoginForm from "../src/components/login-form";
 
 
 class App extends Component {
 
-  
+
   render() {
     return (
       <Router>
@@ -24,15 +24,14 @@ class App extends Component {
             <Route exact path="/search" component={SearchMovies} />
             <Route exact path="/saved" component={SavedMovies} />
             <Route exact path="/settings" component={Settings} />
-          <Route path="/login" render={() => <LoginForm updateUser={Success.updateUser} />} />
-          <Route path="/sign-up" render={() => <Signup/>} />
-            <Route exact path="/" component={Login} />
+            <Route path="/login" render={() => <LoginForm updateUser={Success.updateUser} />} />
+            <Route path="/signup" render={() => <Signup />} />
+            {/* <Route exact path="/" component={Login} /> */}
             <Route component={NoMatch} />
           </Switch>
         </div>
       </Router>
     )
   }
-  }
-  export default App;
-  
+}
+export default App;
